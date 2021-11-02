13e3b431bc8dbe4f30fd5e3500511dedab1c5262
Taylorshuang <taylorshuang@163.com>
2021-11-02 02:25:51 -0700

    complete the three homework

--- computeArea/src/main.rs
+++ computeArea/src/main.rs
@@ -1,19 +1,54 @@
+use std::pin;
+
 fn main() {
-    println!("Hello, world!");
+
+    let triangle = Triangle{
+        side:40,
+        altitude:30,
+    };
+    println!("The area of this triangle is {}",triangle.area());
+
+    let rectangle = Rectangle{
+        long: 20,
+        width: 50,
+    };
+    println!("The area of this rectangle is {}",rectangle.area());
+
+    compute_area(triangle);
+    compute_area(rectangle);
 }
+
+pub trait Area {
+    fn area(&self)->u32;
+}
+
 
 struct Triangle{
     side: u32,
     altitude: u32,
 } 
 
-impl Triangle {
+impl Area for Triangle {
     fn area(&self) -> u32{
-        1/2 * self.side * self.altitude
+        let area=  (self.side * self.altitude)/2;
+        area
     }
 }
 
-//Compute area
-fn compute_area<T>(len:<T>)->T{
+struct Rectangle{
+    long: u32,
+    width: u32,
+}
 
+impl Area for Rectangle{
+    fn area(&self) -> u32{
+        let area =self.long * self.width;
+        area
+    }
+}
+
+fn compute_area<T:Area>(any:T){
+    println!("we can compute area of triangle and rectangle");
+    let area= any.area();
+    println!("the area of this tangle is {}",area);
 }
